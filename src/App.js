6b9c60e039a01d9f7844a49466eda260be2a9bc6
Furkan Kırmızıoğlu <kirmizioglufurkan@gmail.com>
2023-05-14 23:46:58 +0300

    14/05/2023 commit. API endpoints have changed. API call & response corrections have done. From now Python back-end has been deprecated.

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ export default function App() {
   const [valuation, setValuation] = useState();
 
   useEffect(() => {
-    document.title = 'AlgoAnalyze'
+    document.title = "AlgoAnalyze";
     const fetchData = async () => {
-      const response = await fetch("http://localhost:8000/getIndustries", { mode: 'cors' });
+      const response = await fetch("http://localhost:8080/industry/list", {
+        mode: "cors",
+      });
       const data = await response.json();
       setIndustries(data);
     };
@@ -18,34 +20,37 @@ export default function App() {
   }, []);
 
   const fetchValuation = async (industry) => {
-    const response = await fetch(`http://localhost:8000/valuation?industry=${encodeURIComponent(industry)}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": '*',
-        },
-      }
-    );
+    const response = await fetch(`http://localhost:8080/valuation/list`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        "industry": industry,
+      }),
+    });
     const data = await response.json();
     setValuation(data);
   };
 
   return (
     <div>
-      
       <header class="p-3 text-bg-dark">
         <div class="container">
           <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
               <li>
-                <h4>AlgoAnalyze - Fundamental Analysis Application For Istanbul Stock Exchange</h4>
+                <h4>
+                  AlgoAnalyze - Fundamental Analysis Application For Istanbul
+                  Stock Exchange
+                </h4>
               </li>
             </ul>
           </div>
         </div>
       </header>
-      
+
       <div class="container col-xl-14 col-xxl-14">
         <div class="row align-items-center g-lg-5 py-5">
           <div class="col-lg-7 text-center text-lg-start">
@@ -53,9 +58,10 @@ export default function App() {
               Choosing stocks has never been easier!
             </h1>
             <p class="col-lg-10 fs-4">
-              Various metrics such as PEG Ratio, Price / Book Value and more has scored simultaneously and 
-              bringing opportunities of stock market in to your screen.
-              You can find the stocks that have the highest chance of profit by choosing a business next to this section.
+              Various metrics such as PEG Ratio, Price / Book Value and more has
+              scored simultaneously and bringing opportunities of stock market
+              in to your screen. You can find the stocks that have the highest
+              chance of profit by choosing a business next to this section.
             </p>
           </div>
           <div class="col-md-10 mx-auto col-lg-5">
@@ -67,10 +73,11 @@ export default function App() {
                     <li>
                       <button
                         class="dropdown-item d-flex align-items-center gap-2 py-2"
-                        onClick={() => fetchValuation(indObj.industry)}
-                        key={index}>
+                        onClick={() => fetchValuation(industries[index])}
+                        key={index}
+                      >
                         <span class="d-inline-block bg-danger rounded-circle p-1"></span>
-                        {indObj.industry}
+                        {industries[index]}
                       </button>
                     </li>
                   ))}
@@ -79,7 +86,7 @@ export default function App() {
           </div>
         </div>
       </div>
-      
+
       <div class="container col-xl-14 col-xxl-14">
         <table class="table table-striped table-dark">
           <thead>
@@ -98,17 +105,17 @@ export default function App() {
           <tbody>
             {valuation &&
               valuation.length > 0 &&
-              valuation.map((indObj, index) => (
+              valuation.map((value) => (
                 <tr>
-                  <th scope="row">{indObj[0]}</th>
-                  <td>{indObj[1]}</td>
-                  <td>{indObj[2]}</td>
-                  <td>{indObj[3]}</td>
-                  <td>{indObj[4]}</td>
-                  <td>{indObj[5]}</td>
-                  <td>%{indObj[6]}</td>                  
-                  <td>{indObj[7]}</td>
-                  <td>{indObj[8]}</td>
+                  <th scope="row">{value.ticker}</th>
+                  <td>{value.companyName}</td>
+                  <td>{value.latestBalanceSheetTerm}</td>
+                  <td>{value.price}</td>
+                  <td>{value.pb}</td>
+                  <td>{value.peg}</td>
+                  <td>%{value.longTermDebtToEquity}</td>
+                  <td>{value.finalScore}</td>
+                  <td>{value.suggestion}</td>
                 </tr>
               ))}
           </tbody>
@@ -117,12 +124,10 @@ export default function App() {
 
       <div class="container">
         <footer class="py-3 my-4">
-          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-          </ul>
+          <ul class="nav justify-content-center border-bottom pb-3 mb-3"></ul>
           <p class="text-center text-muted">© 2023 Furkan Kırmızıoğlu</p>
         </footer>
       </div>
-
     </div>
   );
 }
